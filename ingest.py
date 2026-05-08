392cf24f2697b348bc18ebb467f397cae6f51670
worldpactleader <ivanstetsenko48@gmail.com>
2026-05-08 18:43:36 +0300

    Feat: добавлена обработка PDF и создание локальной векторной базы FAISS

---
+++ ingest.py
@@ -0,0 +1,21 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+def process_pdf(file_path):
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+    
+    vectorstore = FAISS.from_documents(documents=splits, embedding=embeddings)
+    vectorstore.save_local("faiss_index")
+    print("Документ обработан и сохранен в базу")
+
+if __name__ == "__main__":
+    process_pdf("исландия - Поиск в Google.pdf")
